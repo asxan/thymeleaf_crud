6f583bcb94e7b0d3695a0908e254a6ea3fbbfb7b
Vitalii Klymov <klimov09032000@gmail.com>
2024-02-09 11:01:10 +0200

    DEVOPS-1 Add Company service and controller

---
+++ src/main/java/com/tl/crud/service/CompanyService.java
@@ -0,0 +1,6 @@
+package com.tl.crud.service;
+
+import com.tl.crud.entity.Company;
+
+public interface CompanyService extends CrudService<Company>{
+}
---
+++ src/main/java/com/tl/crud/util/ExceptionUtil.java
@@ -0,0 +1,8 @@
+package com.tl.crud.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionUtil {
+    public static final String ENTITY_NOT_FOUND_EXCEPTION = "Entity was not found";
+}
